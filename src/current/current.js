b9ba907003f810590e405ca0ed537f5244c68e95
Jaka Černetič <jaka.cernetic@students.vegova.si>
2024-01-15 21:59:04 +0100

    mal dela na forecastu

--- src/current/current.js
+++ src/current/current.js
@@ -11,7 +11,7 @@ function Current() {
 				const apiKey = "&key=" + key.name;
 				const response = await fetch(
 					"https://api.weatherapi.com/v1/current.json?q=" +
-						"Vrhnika" +
+						"Sydney" +
 						apiKey
 				);
 				const data = await response.json();
@@ -26,90 +26,8 @@ function Current() {
 	}, []);
 
 	if (!data) {
-		return null; // or render a loading state
+		return null;
 	}
-
-	const decodeWindDirection = (windDirection) => {
-		if ((windDirection = "N")) {
-			return "Sever";
-		} else if ((windDirection = "NNE")) {
-			return "Sever-severovzhod";
-		} else if ((windDirection = "NE")) {
-			return "Severovzhod";
-		} else if ((windDirection = "ENE")) {
-			return "Vzhod-severovzhod";
-		} else if ((windDirection = "E")) {
-			return "Vzhod";
-		} else if ((windDirection = "ESE")) {
-			return "Vzhod-jugovzhod";
-		} else if ((windDirection = "SE")) {
-			return "Jugovzhod";
-		} else if ((windDirection = "SSE")) {
-			return "Jug-jugovzhod";
-		} else if ((windDirection = "S")) {
-			return "Jug";
-		} else if ((windDirection = "SSW")) {
-			return "Jug-jugozahod";
-		} else if ((windDirection = "SW")) {
-			return "Jugozahod";
-		} else if ((windDirection = "WSW")) {
-			return "Zahod-jugozahod";
-		} else if ((windDirection = "W")) {
-			return "Zahod";
-		} else if ((windDirection = "WNW")) {
-			return "Zahod-severozahod";
-		} else if ((windDirection = "NW")) {
-			return "Severozahod";
-		} else if ((windDirection = "NNW")) {
-			return "Sever-severozahod";
-		}
-	};
-
-	const decodeCondition = (condition) => {
-		if ((condition = "Sunny")) {
-			return "Sončno";
-		} else if ((condition = "Partly cloudy")) {
-			return "Delno oblačno";
-		} else if ((condition = "Cloudy")) {
-			return "Oblačno";
-		} else if ((condition = "Overcast")) {
-			return "Pretežno oblačno";
-		} else if ((condition = "Mist")) {
-			return "Megleno";
-		} else if ((condition = "Patchy rain possible")) {
-			return "Možnost padavin";
-		} else if ((condition = "Patchy snow possible")) {
-			return "Možnost sneženja";
-		} else if ((condition = "Patchy sleet possible")) {
-			return "Možnost dežja";
-		} else if ((condition = "Patchy freezing drizzle possible")) {
-			return "Možnost pozebe";
-		} else if ((condition = "Thundery outbreaks possible")) {
-			return "Možnost neviht";
-		} else if ((condition = "Blowing snow")) {
-			return "Sneži";
-		} else if ((condition = "Blizzard")) {
-			return "Snežni vihar";
-		} else if ((condition = "Fog")) {
-			return "Megla";
-		} else if ((condition = "Freezing fog")) {
-			return "Zmrznjena megla";
-		} else if ((condition = "Patchy light drizzle")) {
-			return "Rahel dež";
-		} else if ((condition = "Light drizzle")) {
-			return "Rahel dež";
-		} else if ((condition = "Freezing drizzle")) {
-			return "Zmrznjen dež";
-		} else if ((condition = "Heavy freezing drizzle")) {
-			return "Močan zmrznjen dež";
-		} else if ((condition = "Patchy light rain")) {
-			return "Rahel dež";
-		} else if ((condition = "Light rain")) {
-			return "Rahel dež";
-		} else if ((condition = "Moderate rain at times")) {
-			return "Zmeren dež";
-		}
-	};
 
 	return (
 		<div className="display">
@@ -123,20 +41,17 @@ function Current() {
 			</div>
 			<div className="condition">
 				<img
-					className="condition__icon"
 					src={data.current.condition.icon}
 					alt={data.current.condition.text}
 				/>
-				<a className="condition__text">
-					{decodeCondition(data.current.condition.text)}
-				</a>
+				<a>{data.current.condition.text}</a>
 			</div>
 			<div className="other">
 				<div className="wind">
 					<a className="other-title">Veter</a>
 					<a>{data.current.wind_kph} km/h</a>
 					<p>{data.current.wind_mph} mph</p>
-					<p>{decodeWindDirection(data.current.wind_dir)}</p>
+					<p>{data.current.wind_dir}</p>
 				</div>
 				<div className="humidity">
 					<a className="other-title">Vlažnost</a>
@@ -154,7 +69,7 @@ function Current() {
 				<div className="visibility">
 					<a className="other-title">Vidljivost</a>
 					<a>{data.current.vis_km} km</a>
-					<p>{data.current.vis_miles} miles</p>
+					<p>{data.current.vis_miles} milj/a</p>
 				</div>
 			</div>
 		</div>
--- src/forecast/forecast.js
+++ src/forecast/forecast.js
@@ -1,8 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./forecast.css";
+import key from "../api.json";
 
 function Forecast() {
-	return <h1>forecast</h1>;
+	const [data, setData] = useState(null);
+
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const apiKey = "&key=" + key.name;
+				const response = await fetch(
+					"https://api.weatherapi.com/v1/forecast.json?q=" +
+						"Sydney" +
+						"&days=3" +
+						apiKey
+				);
+				const data = await response.json();
+				setData(data);
+				console.log(data);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+
+		fetchData();
+	}, []);
+
+	if (!data) {
+		return null;
+	}
+
+	return <div className="display"></div>;
 }
 
 export default Forecast;
