6fa88fc6a85f0d7ef4ae25361a5f449048269ba1
Jaka Černetič <jaka.cernetic@students.vegova.si>
2024-01-30 18:58:37 +0100

    today končan

--- src/today/today.js
+++ src/today/today.js
@@ -28,56 +28,39 @@ const Today = (data = { data }) => {
 
 	return (
 		<div className="display">
-			<div className="top">
-				<div className="topLeft">
-					<div className="location">
-						<a>{display.location.name},</a>
-						<p>{display.location.country}</p>
-					</div>
-					<div className="temp">
-						<a>{display.current.temp_c}°C</a>
-						<p>( {display.current.temp_f}°F )</p>
-					</div>
-					<div className="condition">
-						<img
-							src={display.current.condition.icon}
-							alt={display.current.condition.text}
-						/>
-						<a>{display.current.condition.text}</a>
-					</div>
+			<div>
+				<div className="location">
+					<a>{display.location.name},</a>
+					<p>{display.location.country}</p>
 				</div>
-				<div className="remainingForecast">
-					{renderRemainingForecast(
-						display.current.last_updated.slice(-5)
-					)}
+				<div className="temp">
+					<a>{display.current.temp_c}°C</a>
+					<p>( {display.current.temp_f}°F )</p>
+				</div>
+				<div className="condition">
+					<img
+						src={display.current.condition.icon}
+						alt={display.current.condition.text}
+					/>
+					<a>{display.current.condition.text}</a>
+				</div>
+				<div className="astro">
+					<a>
+						Sončni vzhod ob:{" "}
+						{display.forecast.forecastday[0].astro.sunrise}
+					</a>
+					<a>
+						Sončni zahod ob:{" "}
+						{display.forecast.forecastday[0].astro.sunset}
+					</a>
 				</div>
 			</div>
-			<div className="bottomLeft">
-				<div className="astro">
-					<div className="sunrise">
-						<a>
-							Sončni vzhod ob:{" "}
-							{display.forecast.forecastday[0].astro.sunrise}
-						</a>
-					</div>
-					<div className="sunset">
-						<a>
-							Sončni zahod ob:{" "}
-							{display.forecast.forecastday[0].astro.sunset}
-						</a>
-					</div>
-					<div className="moonphase">
-						<a>
-							Lunina mena:{" "}
-							{display.forecast.forecastday[0].astro.moon_phase}
-						</a>
-					</div>
-					<div className="sunPath">
-						<a>
-							Položaj sonca:{" "}
-							{display.forecast.forecastday[0].astro.sunrise}
-						</a>
-					</div>
+			<div className="remainingForecast">
+				<a>Preostanek dneva:</a>
+				<div className="scrollbar">
+				{renderRemainingForecast(
+					display.current.last_updated.slice(-5)
+				)}
 				</div>
 			</div>
 		</div>
