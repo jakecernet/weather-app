94a7211dc396328315d6187bc51c6af9bf61cea5
Jaka Černetič <jaka.cernetic@gmail.com>
2025-03-15 15:44:12 +0100

    fix routerja

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {
-	BrowserRouter as Router,
-	Route,
-	Routes,
-	NavLink,
-} from "react-router-dom";
+import { HashRouter as Router, Route, Routes, NavLink } from "react-router-dom";
 
 import api from "./api.json";
 import "./App.css";
@@ -26,133 +21,142 @@ import feedback from "./buttons/feedback.svg";
 import menu from "./buttons/menu.svg";
 
 function App() {
-	const [navbar, setNavClosed] = useState(true);
-	const [data, setData] = useState(null);
-	const output_city = localStorage.getItem("city");
-	let cities = [];
+    const [navbar, setNavClosed] = useState(true);
+    const [data, setData] = useState(null);
+    const output_city = localStorage.getItem("city");
+    let cities = [];
 
-	function toggleNav() {
-		setNavClosed(!navbar);
-		document.body.classList.toggle("small-nav");
-		document.body.classList.toggle("avg-nav");
-	}
+    function toggleNav() {
+        setNavClosed(!navbar);
+        document.body.classList.toggle("small-nav");
+        document.body.classList.toggle("avg-nav");
+    }
 
-	const setBackground = () => {
-		const body = document.body;
-		const currentHour = new Date().getHours();
-		if (currentHour >= 6 && currentHour < 19) {
-			body.style.backgroundImage = `url(${Day})`;
-		} else {
-			body.style.backgroundImage = `url(${Night})`;
-		}
-		body.style.backgroundSize = "cover";
-		body.style.backgroundPosition = "center";
-		body.style.backgroundRepeat = "no-repeat";
-		body.style.backgroundAttachment = "fixed";
-	};
+    const setBackground = () => {
+        const body = document.body;
+        const currentHour = new Date().getHours();
+        if (currentHour >= 6 && currentHour < 19) {
+            body.style.backgroundImage = `url(${Day})`;
+        } else {
+            body.style.backgroundImage = `url(${Night})`;
+        }
+        body.style.backgroundSize = "cover";
+        body.style.backgroundPosition = "center";
+        body.style.backgroundRepeat = "no-repeat";
+        body.style.backgroundAttachment = "fixed";
+    };
 
-	useEffect(() => {
-		fetchCity();
-		fetchData();
-	}, [output_city]);
+    useEffect(() => {
+        fetchCity();
+        fetchData();
+    }, [output_city]);
 
-	async function fetchData() {
-		try {
-			const apiKey = "&key=" + api.name;
-			const response = await fetch(
-				"https://api.weatherapi.com/v1/forecast.json?q=" +
-					output_city +
-					"&days=3" +
-					apiKey
-			);
-			const receivedData = await response.json();
-			setData(receivedData);
-		} catch (error) {
-			console.error(error);
-		}
-	}
+    async function fetchData() {
+        try {
+            const apiKey = "&key=" + api.name;
+            const response = await fetch(
+                "https://api.weatherapi.com/v1/forecast.json?q=" +
+                    output_city +
+                    "&days=3" +
+                    apiKey
+            );
+            const receivedData = await response.json();
+            setData(receivedData);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
-	async function fetchCity() {
-		if (output_city == null) {
-			const myip = await fetch("https://api.ipify.org?format=json");
-			const ip = await myip.json();
-			const mylocation = await fetch(
-				"https://ipapi.co/" + ip.ip + "/json/"
-			);
-			const location = await mylocation.json();
-			const city = location.city;
+    async function fetchCity() {
+        if (output_city == null) {
+            const myip = await fetch("https://api.ipify.org?format=json");
+            const ip = await myip.json();
+            const mylocation = await fetch(
+                "https://ipapi.co/" + ip.ip + "/json/"
+            );
+            const location = await mylocation.json();
+            const city = location.city;
 
-			if (localStorage.getItem("city") == null) {
-				localStorage.setItem("city", city);
-				let cities = JSON.parse(localStorage.getItem("cities")) || [];
-				cities.push(city);
-				localStorage.setItem("cities", JSON.stringify(cities));
-			}
-		}
-	}
+            if (localStorage.getItem("city") == null) {
+                localStorage.setItem("city", city);
+                let cities = JSON.parse(localStorage.getItem("cities")) || [];
+                cities.push(city);
+                localStorage.setItem("cities", JSON.stringify(cities));
+            }
+        }
+    }
 
-	if (!data) {
-		return <div>{<Loading />}</div>;
-	}
+    if (!data) {
+        return <div>{<Loading />}</div>;
+    }
 
-	return (
-		<div onLoad={setBackground()}>
-			<Router>
-				<nav>
-					<ul>
-						<NavLink to="/" style={{ textDecoration: "none" }}>
-							<li>
-								<img src={home} alt="home" />
-								{navbar ? null : <a>Trenutno</a>}
-							</li>
-						</NavLink>
-						<NavLink to="/today" style={{ textDecoration: "none" }}>
-							<li>
-								<img src={today} alt="today" />
-								{navbar ? null : <a>Danes</a>}
-							</li>
-						</NavLink>
-						<NavLink to="/forecast" style={{ textDecoration: "none" }}>
-							<li>
-								<img src={forecast} alt="forecast" />
-								{navbar ? null : <a>Napoved</a>}
-							</li>
-						</NavLink>
-						<NavLink to="/settings" style={{ textDecoration: "none" }}>
-							<li>
-								<img src={settings} alt="settings" />
-								{navbar ? null : <a>Nastavitve</a>}
-							</li>
-						</NavLink>
-					</ul>
-					<div className="bottom">
-						<NavLink to="/feedback" style={{ textDecoration: "none" }}>
-							<li className="button">
-								<img src={feedback} alt="feedback" />
-								{navbar ? null : <a>Vaše mnenje</a>}
-							</li>
-						</NavLink>
-						<li className="button" onClick={toggleNav}>
-							<img src={menu} alt="menu" />
-							{navbar ? null : <a>Skrči</a>}
-						</li>
-					</div>
-				</nav>
-				<div className="container">
-					<Routes>
-						<Route path="/" element={<Current data={data} />} />
-						<Route path="/today" element={<Today data={data} />} />
-						<Route
-							path="/forecast"
-							element={<Forecast data={data} />}
-						/>
-						<Route path="/settings" element={<Settings />} />
-						<Route path="/feedback" element={<Feedback />} />
-					</Routes>
-				</div>
-			</Router>
-		</div>
-	);
+    return (
+        <div onLoad={setBackground()}>
+            <Router>
+                <nav>
+                    <ul>
+                        <NavLink to="/" style={{ textDecoration: "none" }}>
+                            <li>
+                                <img src={home} alt="home" />
+                                {navbar ? null : <a>Trenutno</a>}
+                            </li>
+                        </NavLink>
+                        <NavLink to="/today" style={{ textDecoration: "none" }}>
+                            <li>
+                                <img src={today} alt="today" />
+                                {navbar ? null : <a>Danes</a>}
+                            </li>
+                        </NavLink>
+                        <NavLink
+                            to="/forecast"
+                            style={{ textDecoration: "none" }}
+                        >
+                            <li>
+                                <img src={forecast} alt="forecast" />
+                                {navbar ? null : <a>Napoved</a>}
+                            </li>
+                        </NavLink>
+                        <NavLink
+                            to="/settings"
+                            style={{ textDecoration: "none" }}
+                        >
+                            <li>
+                                <img src={settings} alt="settings" />
+                                {navbar ? null : <a>Nastavitve</a>}
+                            </li>
+                        </NavLink>
+                    </ul>
+                    <div className="bottom">
+                        <NavLink
+                            to="/feedback"
+                            style={{ textDecoration: "none" }}
+                        >
+                            <li className="button">
+                                <img src={feedback} alt="feedback" />
+                                {navbar ? null : <a>Vaše mnenje</a>}
+                            </li>
+                        </NavLink>
+                        <li className="button" onClick={toggleNav}>
+                            <img src={menu} alt="menu" />
+                            {navbar ? null : <a>Skrči</a>}
+                        </li>
+                    </div>
+                </nav>
+                <div className="container">
+                    <Routes>
+                        <Route path="/" element={<Current data={data} />} />
+                        <Route path="/today" element={<Today data={data} />} />
+                        <Route
+                            path="/forecast"
+                            element={<Forecast data={data} />}
+                        />
+                        <Route path="/settings" element={<Settings />} />
+                        <Route path="/feedback" element={<Feedback />} />
+                    </Routes>
+                </div>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
